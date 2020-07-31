9f1866084fb6fcac9ea037ca5d6829e07d05837a
xbNigel <xbNigel@outlook.com>
2020-08-01 06:15:25 +1000

    July 31st Update

--- R6SUnlockAll/DLLMain.cpp
+++ R6SUnlockAll/DLLMain.cpp
@@ -11,7 +11,7 @@ UINT_PTR UnknowCallBack(UINT_PTR rcx, UINT_PTR rdx, UINT_PTR r8)
 	UINT_PTR Ret = UnknowCallBack_t(rcx, rdx, r8);
 	UINT_PTR RetAddressPtr = (UINT_PTR)_AddressOfReturnAddress();
 
-	if ((UINT_PTR)_ReturnAddress() == (0x1D2ADD9 + GameBaseAddr))
+	if ((UINT_PTR)_ReturnAddress() == (0x27146C9 + GameBaseAddr))
 	{
 		*(UINT_PTR*)(RetAddressPtr) = ((UINT_PTR)_ReturnAddress() - 0x3DA);
 
@@ -33,11 +33,11 @@ BOOL WINAPI DllMain(
 	switch (Reason)
 	{
 	case(DLL_PROCESS_ATTACH):
-		UnknowCallBack_t = (decltype(UnknowCallBack_t))(*(UINT_PTR*)(GameBaseAddr + 0x55643D0));
-		*(UINT_PTR*)(GameBaseAddr + 0x55643D0) = (UINT_PTR)UnknowCallBack;
+		UnknowCallBack_t = (decltype(UnknowCallBack_t))(*(UINT_PTR*)(GameBaseAddr + 0x557DAC0));
+		*(UINT_PTR*)(GameBaseAddr + 0x557DAC0) = (UINT_PTR)UnknowCallBack;
 		break;
 	case(DLL_PROCESS_DETACH):
-		*(UINT_PTR*)(GameBaseAddr + 0x55643D0) = (UINT_PTR)UnknowCallBack_t;
+		*(UINT_PTR*)(GameBaseAddr + 0x557DAC0) = (UINT_PTR)UnknowCallBack_t;
 		break;
 	}
 
